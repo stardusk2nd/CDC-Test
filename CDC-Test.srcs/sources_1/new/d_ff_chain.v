1dc22f92ff7205f33741596670e7d184a0b58c43
Stardust <unite0018@gmail.com>
2025-02-14 18:00:07 +0900

    backup

--- CDC-Test.srcs/sources_1/new/d_ff_chain.v
+++ CDC-Test.srcs/sources_1/new/d_ff_chain.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 `include "config.vh"
 
 module d_ff_chain(
@@ -7,35 +6,27 @@ module d_ff_chain(
     `ifdef SYNC_RST
     input rst_n,
     `endif
-    input d,
-    output q
+    input i_async,
+    output o_sync
     );
     
-    wire q_a;
-    wire [1:0] q_b;
+    wire [2:0] q;
+    assign o_sync = q[2];
     
     `ifdef SYNC_RST
-    d_ff_rst d_ff_a(
-        clk_a, rst_n, d, q_a
-    );
-    d_ff_rst d_ff_b_0(
-        clk_b, rst_n, q_a, q_b[0]
-    );
-    d_ff_rst d_ff_b_1(
-        clk_b, rst_n, q_b[0], q_b[1]
-    );
+        d_ff_rst d_ff_a(
+            clk_a, rst_n, d, q[0]
+        );
+        d_ff_rst d_ff_b[1:0](
+            clk_b, rst_n, q[0], q[1]
+        );
     `else
-    d_ff d_ff_a(
-        clk_a, d, q_a
-    );
-    d_ff d_ff_b_0(
-        clk_b, q_a, q_b[0]
-    );
-    d_ff d_ff_b_1(
-        clk_b, q_b[0], q_b[1]
-    );
+        d_ff d_ff_a(
+            clk_a, d, q[0]
+        );
+        d_ff d_ff_b[1:0](
+            clk_b, q[0], q[1]
+        );
     `endif
-    
-    assign q = q_b[1];
     
 endmodule
